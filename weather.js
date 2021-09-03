9e82b3a412cb7bdf8c9b53b8cf1b89fc500c500a
SerguioL <serguio.l.landa@gmail.com>
2021-09-02 20:05:46 -0500

    added map with search bar

--- weather.js
+++ weather.js
@@ -1,11 +1,19 @@
 
 mapboxgl.accessToken = MAPBOX_API_TOKEN;
 const map = new mapboxgl.Map({
-    container: 'map', // container ID
-    style: 'mapbox://styles/mapbox/streets-v11', // style URL
-    center: [-98.4936, 29.4241], // starting position [lng, lat]
-    zoom: 9 // starting zoom
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: [-98.4936, 29.4241],
+    zoom: 10
 });
+
+// Add the control to the map.
+map.addControl(
+    new MapboxGeocoder({
+        accessToken: mapboxgl.accessToken,
+        mapboxgl: mapboxgl
+    })
+);
 
 // Add zoom and rotation controls to the map.
 map.addControl(new mapboxgl.NavigationControl());
